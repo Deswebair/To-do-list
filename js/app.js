71dc87f355f9363e943362a27078b21c95e31ccf
gut7y <deswebproy@gmail.com>
2022-05-31 18:34:11 -0500

    arreglos
    cambio de nombres a la s variables

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// Variables
+const formulario = document.querySelector('#formulario');
+const listaTweets = document.querySelector('#lista-tweets');
+let tweets = [];
+
+
+//Event Listeners
+eventListeners();
+
+function eventListeners() {
+    formulario.addEventListener('submit', agregarTweet);
+}
+
+
+// Funciones
+function agregarTweet(e) {
+    e.preventDefault();
+
+    // Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').ariaValueMax;
+
+
+    // Validacion..
+    if(tweet === ''){
+        mostrarError('un mensaje no puede ir vacio')
+        return; // evita que se ejecuten mas lineas
+    }
+}
+
+// Mostrar mensaje de error
+function mostrarError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+
+    // Insertarlo en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+}
